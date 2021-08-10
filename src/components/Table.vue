14c8446d47142e3de1dd36cb8c716e150737e6bf
Austin Asbury <austin.asbury@ritterim.com>
2021-08-10 16:03:08 -0400

    pull data in component

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="image-container">
     <img 
       src="https://image.shutterstock.com/z/stock-photo-the-business-people-thumb-up-at-the-table-view-from-above-718092013.jpg"
       class="table">
     <div v-for="developer in developers" :key="developer.name">
-      <developer></developer>
+      <developer :name="developer.name" :picture="developer.picture"></developer>
     </div>
   </div>
 </template>
@@ -17,8 +17,8 @@ export default {
   name: 'Table',
   created() {
     this.$store.dispatch('getDevelopers')
-      .then(() => {
-
+      .then(res => {
+        this.developers = res.data.developers;
       })
   },
   data() {
